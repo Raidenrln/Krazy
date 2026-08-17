8eb16870433c70e79f5ca13a0fb61791ced6f04f
Roluna <Christiandelroluna19@gmail.com>
2026-08-17 17:33:43 +0800

    a

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import "@fontsource/inter";
 import "@fontsource/jetbrains-mono";
 import "@fontsource/press-start-2p";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
+   <StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
